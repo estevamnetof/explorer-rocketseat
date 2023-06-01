37deb3aedeec6971f72460cde65ecb38141afe6e
estevamnetof <estevam.neto07@gmail.com>
2023-06-01 14:32:41 -0300

    focusTimer adicionado

--- stage-05/focusTimer/js/timer.js
+++ stage-05/focusTimer/js/timer.js
@@ -1,3 +1,5 @@
+import Sounds from './sounds.js' 
+
 export default function Timer({ 
     minutesDisplay, 
     secondsDisplay, 
@@ -31,6 +33,7 @@ export default function Timer({
         if (isFinished) {
           resetControls()
           updateDisplay()
+          Sounds().timeEnd()
           return
         }
   
